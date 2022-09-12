939070dc5f4e8cb7c0f564c484535a004e65d93f
Allen Guo <alleng@graphcore.ai>
2022-09-12 21:28:33 +0800

    wip

--- t.sh
+++ t.sh
@@ -1,4 +1,4 @@
-# cmake config 
+# cmake config
 
 # cmake build all/paddle_python with dbg libs
 
@@ -7,3 +7,9 @@ cp build/paddle/fluid/pybind/libpaddle_pybind.so python/paddle/fluid/core_avx.so
 cp -r build/python/paddle/fluid/proto/ python/paddle/fluid/
 cp -r build/python/paddle/version python/paddle/
 cp -r build/python/paddle/distributed/fleet/proto/ python/paddle/distributed/fleet
+
+# temp
+rm -rf python/paddle/fluid/core_avx.so
+rm -rf python/paddle/fluid/proto
+rm -rf python/paddle/version
+rm -rf python/paddle/distributed/fleet/proto
