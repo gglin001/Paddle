f8ce98ce5e5d85a9d4ee28717cc8d0bc69939211
Allen Guo <guosonglin001@gmail.com>
2023-06-09 04:47:12 +0000

    add stubgen.sh

---
+++ demos/stubgen.sh
@@ -0,0 +1,37 @@
+# cmake --build /repos/gglin001/Paddle/build --config Release --target all
+apt install patch patchelf
+
+pip install -r python/requirements.txt
+pip install mypy
+
+# stubgen \
+#     -m paddle.fluid.libpaddle \
+#     -m paddle.fluid.libpaddle.eager \
+#     -o build/python
+# stubgen \
+#     -m paddle.fluid.core \
+#     -o build/python
+
+# cmake --build `pwd`/build --config Release --target paddle
+cp build/paddle/fluid/pybind/libpaddle.so python/paddle/fluid/
+
+cp -r build/python/paddle/fluid/proto python/paddle/fluid/
+cp -r build/python/paddle/distributed/fleet/proto python/paddle/distributed/fleet/
+cp -r build/python/paddle/version python/paddle
+
+stubgen \
+    -m paddle.fluid.libpaddle \
+    -m paddle.fluid.libpaddle.eager \
+    -m paddle.fluid.libpaddle.eager.ops \
+    -m paddle.fluid.libpaddle.eager.ops.legacy \
+    -o python \
+    -v
+
+echo "from . import eager" >>python/paddle/fluid/libpaddle/__init__.pyi
+echo "from . import ops" >>python/paddle/fluid/libpaddle/eager/__init__.pyi
+echo "from . import legacy" >>python/paddle/fluid/libpaddle/eager/ops/__init__.pyi
+
+# no need
+# stubgen \
+#     -m paddle.fluid.core \
+#     -o python
