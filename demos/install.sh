b350f279e9122765c96230867d993df9aaf7bd85
Allen Guo <alleng@graphcore.ai>
2023-09-28 05:27:42 +0000

    wip

--- demos/install.sh
+++ demos/install.sh
@@ -4,6 +4,7 @@ pip install -r python/requirements.txt
 cp -rf build/python/paddle/base/proto python/paddle/base/
 cp -rf build/python/paddle/distributed/fleet/proto python/paddle/distributed/fleet/
 cp -rf build/python/paddle/version python/paddle
+cp build/python/paddle/cuda_env.py python/paddle/cuda_env.py
 
 cp build/python/paddle/base/libpaddle.so python/paddle/base/libpaddle.so
 cp -rf build/python/paddle/libs python/paddle/
